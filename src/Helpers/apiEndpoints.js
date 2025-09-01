c1965d70087b0fdca5e795057e56ea68ca2cbfe7
emenac-muneeb <muneeb.javed@emenac.co.uk>
2025-09-01 19:23:18 +0500

    Resolve merge conflicts

--- src/Helpers/apiEndpoints.js
+++ src/Helpers/apiEndpoints.js
@@ -1,13 +1,41 @@
+<<<<<<< Updated upstream
 // const BASE_URL = 'http://makkah-travel-uk-admin.test/public/api'
 const BASE_URL = "https://admin.makkahtravel.co.uk/api";
 const BASE_URL_IMG = "https://admin.makkahtravel.co.uk/media";
 const BASE_URL_SVG = "https://admin.makkahtravel.co.uk/";
 
+=======
+// const BASE_URL = "http://makkah-travel-uk-admin.test/public/api";
+// const BASE_URL_IMG = "http://makkah-travel-uk-admin.test/public/media";
+// const BASE_URL_SVG = "http://makkah-travel-uk-admin.test/public/";
+const BASE_URL = "https://admin.makkahtravel.co.uk/api";
+const BASE_URL_IMG = "https://admin.makkahtravel.co.uk/media";
+const BASE_URL_SVG = "https://admin.makkahtravel.co.uk/";
+>>>>>>> Stashed changes
 const endpoints = {
   getPage: `${BASE_URL}/get-page`,
   getPageUrl: (page_url) => `${BASE_URL}/get-page?page_url=${page_url}`,
   sendEmail: `${BASE_URL}/send-email`,
   generalSettings: `${BASE_URL}/general-setting`,
+<<<<<<< Updated upstream
+=======
+  getUmrah: `${BASE_URL}/umrah-packages`,
+  getHajj: `${BASE_URL}/hajj-packages`,
+  umrahByType: (type) => `${BASE_URL}/umrah-packages?type=${type}`,
+  umrahByslug: (slug) => `${BASE_URL}/umrah-packages?slug=${slug}`,
+  umrahById: (id) => `${BASE_URL}/umrah-packages?package_ids=${id}`,
+  umrahByStar: (stars, type) =>
+    `${BASE_URL}/umrah-packages?type=${type}&stars=${stars}`,
+  hajjByType: (type) => `${BASE_URL}/hajj-packages?type=${type}`,
+  hajjByslug: (slug) => `${BASE_URL}/hajj-packages?slug=${slug}`,
+  hajjById: (id) => `${BASE_URL}/hajj-packages?package_ids=${id}`,
+  hajjByStar: (stars, type) =>
+    `${BASE_URL}/hajj-packages?type=${type}&stars=${stars}`,
+  blogpage: `${BASE_URL}/get-makkah-blog`,
+blogdeatilsgpage: (page_url) => `${BASE_URL}/get-makkah-blog?page_url=${page_url}`,
+
+
+>>>>>>> Stashed changes
 };
 
 export { BASE_URL, endpoints, BASE_URL_IMG, BASE_URL_SVG };
