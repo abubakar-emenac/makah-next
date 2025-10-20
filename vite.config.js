58b505bf2ab8cf1bd7cd652687a4668b8303de11
aliraza910 <prod.ali9100@gmail.com>
2025-10-20 15:16:29 +0500

    favicon updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import axios from "axios";
 import { BASE_URL_SVG_PUBLIC, endpoints } from "./src/Helpers/apiEndpoints";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
